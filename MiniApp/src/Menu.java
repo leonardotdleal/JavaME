14ee7841b9cada04ac3461ce13dc8af455e890ce
Leonardo <leonardotade@gmail.com>
2017-08-28 20:14:47 -0300

    second commit

--- MiniApp/src/Menu.java
+++ MiniApp/src/Menu.java
@@ -1,10 +1,20 @@
+import javax.microedition.lcdui.ChoiceGroup;
 import javax.microedition.lcdui.Form;
 
 
 public class Menu extends Form {
+	
+	private ChoiceGroup choise;
 
 	public Menu(String title) {
 		super(title);
+		
+		choise = new ChoiceGroup("Selecione uma opção:", 
+					ChoiceGroup.EXCLUSIVE,
+					new String[]{"Opção 1","Opção 2"},
+					null);
+		
+		append(choise);
 	}
 
 }
